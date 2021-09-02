3917949eb67098de0cb72f489cc31b65300e0951
andhremaia <andhremaia@gmail.com>
2021-09-02 15:17:42 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,24 +29,20 @@ public class Program {
 			System.out.println("Reservation " + reservation);
 			
 			System.out.println();
-			System.out.println("Ente data to update the reservation: ");
+			System.out.println("Entre data to update the reservation: ");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
+						
+			String error = reservation.updateDates(checkIn, checkOut);	
 			
-			Date now = new Date();
-			
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
 			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			else {
+				System.out.println("Reservation: " + reservation);
 			}
-			else {			
-				reservation.updateDates(checkIn, checkOut);			
-				System.out.println(reservation);
-			}			
 		}
 		sc.close();
 	}
